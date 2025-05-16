93828102cee1f30b6f3c2645aa764880031e5ebf
5to Bachillerato B <5to Bachillerato B@DESKTOP-FF54FBD>
2025-05-16 12:06:37 -0600

    Agregar archivos de proyecto.

---
+++ Inicio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaso_2do_bloque_JR
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Repaso_1 repaso_1 = new Repaso_1();
+            repaso_1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Repaso_2 repaso_2 = new Repaso_2();
+            repaso_2.Show();
+        }
+    }
+}
